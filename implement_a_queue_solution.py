8c13a8e8ec6abb46a16569cdb5d2e996a9c7bf7a
j1wu <ji.wu@outlook.com>
2017-06-08 22:55:43 +0900

    add solution of implementing queue

---
+++ implement_a_queue_solution.py
@@ -0,0 +1,35 @@
+class Queue(object):
+  """
+  * Check if Queue is Empty
+  * Enqueue
+  * Dequeue
+  * Return the size of the Queue
+  """
+  def __init__(self):
+    self.items = []
+
+  def empty(self):
+    return self.items == []
+
+  def enqueue(self, item):
+    self.items.append(item)
+
+  def dequeue(self):
+    return self.items.pop(0)
+
+  def size(self):
+    return len(self.items)
+
+  def show(self):
+    return self.items
+
+
+queue = Queue()
+
+print(queue.empty())
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+queue.dequeue()
+print(queue.show())
