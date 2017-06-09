bce17b275dcd2954b1ec1297b4adb41aaed028d1
j1wu <ji.wu@outlook.com>
2017-06-09 22:32:37 +0900

    add solution of implementing the deque, with left as its front

---
+++ implement_a_deque_solution.py
@@ -0,0 +1,46 @@
+class Deque(object):
+  """
+  * Check if its empty
+  * Add to both front and rear
+  * Remove from Front and Rear
+  * Check the Size
+  """
+  def __init__(self):
+    self.items = []
+
+  def empty(self):
+    return self.items == []
+
+  def size(self):
+    return len(self.items)
+
+  def add_front(self, item):
+    self.items.insert(0, item)
+
+  def add_rear(self, item):
+    self.items.append(item)
+
+  def remove_front(self):
+    self.items.pop(0)
+
+  def remove_rear(self):
+    self.items.pop()
+
+  def show(self):
+    return self.items
+
+deque = Deque()
+
+print(deque.empty())
+deque.add_front(1)
+deque.add_front(0)
+print(deque.show())
+
+deque.add_rear(2)
+print(deque.show())
+
+deque.remove_front()
+print(deque.show())
+
+deque.remove_rear()
+print(deque.show())
