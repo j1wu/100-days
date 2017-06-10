cd35a3d142e4ecdc0031fabee8d1b06360517842
j1wu <ji.wu@outlook.com>
2017-06-10 12:09:28 +0900

    add longest substring problem and my solution

---
+++ longest_substring.py
@@ -0,0 +1,14 @@
+import doctest
+
+def longest_substring(s):
+  """
+  Prints the longest substring of s in which the letters occur in alphabetical order.
+
+  >>> longest_substring('azcbobobegghakl')
+  Longest substring in alphabetical order is: beggh
+  """
+  pass
+
+
+if __name__ == '__main__':
+  doctest.testmod()
---
+++ longest_substring_solution.py
@@ -0,0 +1,29 @@
+import doctest
+
+def longest_substring(s):
+  """
+  Prints the longest substring of s in which the letters occur in alphabetical order.
+
+  >>> longest_substring('azcbobobegghakl')
+  'Longest substring in alphabetical order is: beggh'
+  """
+  first_letter = s[0]
+  last_seen_char_code = ord(first_letter)
+  longest_substr = current_substr = [first_letter]
+
+  for letter in s[1:]:
+    if ord(letter) >= last_seen_char_code:
+      current_substr.append(letter)
+    else:
+      current_substr = [letter]
+
+    last_seen_char_code = ord(letter)
+
+    if len(current_substr) > len(longest_substr):
+      longest_substr = current_substr
+
+  return 'Longest substring in alphabetical order is: ' + ''.join(longest_substr)
+
+
+if __name__ == '__main__':
+  doctest.testmod()
